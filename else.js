0541c655482c5def741019c5c0028d8a674e89fd
Aryan <chaudharyaryan3456@gmail.com>
2024-10-23 14:12:36 +0530

    updated

---
+++ else.js
@@ -0,0 +1,17 @@
+let age = 17;
+if(age >= 18){
+    console.log("you can vote");
+} else{
+    console.log("You cannot vote");
+}
+
+let color = "white";
+if(color === "red"){
+    console.log("stop");
+}else if(color === "yellow"){
+   console.log("ready"); 
+}else if(color === "red"){
+    console.log("go");
+}else {
+    console.log("The light is broken.See yourself and then go.");
+}
---
+++ elseif.js
@@ -0,0 +1,18 @@
+// let age = 14;
+// if(age >= 18){
+//     console.log("you can vote");
+// }else if(age >= 18){
+//     console.log("You cannot vote");
+// }else if(age < 18){
+//     console.log("you cannot vote");
+// }
+
+let marks = 25;
+
+if(marks >= 80){
+    console.log("A+");
+}else if(marks >= 60){
+    console.log("A");
+}else if(marks >= 33){
+    console.log("B");
+}
