14b219cde28ac975e8cdb8e0d24d3424d3476758
Aryan <chaudharyaryan3456@gmail.com>
2024-10-23 14:58:35 +0530

    updated

---
+++ practiceqs4.js
@@ -0,0 +1,26 @@
+// Question - Use switch statement to print the day of the week using a number variable "day" with values 1 to 7.
+
+let day = 5;
+switch(day){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thurdsay");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    default :
+        console.log("Sunday");
+        break;
+}
