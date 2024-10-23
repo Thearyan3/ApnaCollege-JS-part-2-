5418d0f921c322dc182e73f261a6b3217e8240d9
Aryan <chaudharyaryan3456@gmail.com>
2024-10-23 14:42:09 +0530

    updated

---
+++ practiceqs3.js
@@ -0,0 +1,16 @@
+// Question No. 1 - A "good string" is a stirng that starts with the letter 'a' & has a length > 3. Write a Program to find if a stirng is good or not. 
+
+// let str = "pry";
+// if((str[0] === "A" || str[0] === "a") && str.length > 3){
+//     console.log("Good String");
+// }else {
+//     console.log("Bad String");
+// }
+
+// Question No. 2 - Predict the output of following code: 
+ let num = 12;
+ if((num % 3 === 0) && ((num + 1 == 15) || (num - 1 == 11))){
+    console.log("safe");
+ }else{
+    console.log("unsafe");
+ }
