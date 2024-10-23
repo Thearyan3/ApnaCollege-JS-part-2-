4977a90f2b6b21ca22f7357c0b1f1e85d843231e
Aryan <chaudharyaryan3456@gmail.com>
2024-10-23 15:08:17 +0530

    updated

---
+++ alertsprompts.js
@@ -0,0 +1,13 @@
+// alert("Something is wrong.");
+// console.log("this is a simple log");
+// console.error("this is a simple error");
+// console.warn("this is a warning sign");
+
+// let firstName = prompt("enter your name : ");
+// console.log(firstName);
+
+let firstName = prompt("Enter first name : ");
+let lastName = prompt("Enter last name : ");
+let msg = "Welcome " + firstName + " " + lastName + " !";
+
+alert(msg);
