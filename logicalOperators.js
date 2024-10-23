b6ce8d1f0ff6a2008e4f98adf8cb08e6fbe746dd
Aryan <chaudharyaryan3456@gmail.com>
2024-10-23 14:33:13 +0530

    updated

---
+++ logicalOperators.js
@@ -0,0 +1,20 @@
+//Logical AND
+// let marks = 90;
+// if(marks >= 33 && marks >= 80){
+//         console.log("Pass");
+//         console.log("A+");
+// }
+
+// Logical OR 
+// let marks = 90;
+// if(marks >= 33 || marks >= 80){
+//         console.log("Pass");
+//         console.log("A+");
+// }
+
+// Logical NOT 
+let marks = 90;
+if(!(marks < 33)){
+        console.log("Pass");
+        console.log("A+");
+}
